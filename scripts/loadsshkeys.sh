84cd3a615f64cb167b88fafb9185850a66bdd959
git-wwm <73557322+billmoz@users.noreply.github.com>
2025-02-21 16:11:09 -0500

    fixed formatting in loadsshkeys.sh

--- scripts/loadsshkeys.sh
+++ scripts/loadsshkeys.sh
@@ -2,8 +2,8 @@
 # run this script using dot sourcing
 # Example1: . ./loadsshkeys.sh
 # Example2: source ./loadsshkeys.sh
-
 PRIVATE_SSHKEY=/path/to/private-ssh-key
+
 pkill ssh-agent
 eval "$(ssh-agent -s)"
 ssh-add $PRIVATE_SSHKEY
