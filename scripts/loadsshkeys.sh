f3315f47ca6663f6725375330b2265337c25b3b3
bill <73557322+billmoz@users.noreply.github.com>
2025-02-21 16:06:27 -0500

    Create loadsshkeys.sh
    Added file to repo

---
+++ scripts/loadsshkeys.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# run this script using dot sourcing
+# Example1: . ./loadsshkeys.sh
+# Example2: source ./loadsshkeys.sh
+
+PRIVATE_SSHKEY=/path/to/private-ssh-key
+pkill ssh-agent
+eval "$(ssh-agent -s)"
+ssh-add $PRIVATE_SSHKEY
